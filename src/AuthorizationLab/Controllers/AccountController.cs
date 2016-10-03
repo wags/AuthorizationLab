b4c31cfdc41686c077057d62e5986b9273d07c2e
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-03 16:14:47 -0400

    Open up access to login and logout
    Add the "AllowAnonymous" attribute to the Account controller to flag the
    methods as not requiring authentication.

--- src/AuthorizationLab/Controllers/AccountController.cs
+++ src/AuthorizationLab/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.Authentication;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace AuthorizationLab.Controllers
 {
+    [AllowAnonymous]
     public class AccountController : Controller
     {
         public async Task<IActionResult> Login(string returnUrl = null)
