00427e9984a8cf3085a8322aaafd7b3c1018615f
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-04 09:57:19 -0400

    Use the Over21Only policy
    Demonstrate using the policy that enforces an age restriction.

--- src/AuthorizationLab/Controllers/HomeController.cs
+++ src/AuthorizationLab/Controllers/HomeController.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AuthorizationLab.Controllers
 {
-    [Authorize(Policy = "AdministratorOnly")]
-    [Authorize(Policy = "EmployeeId")]
+    [Authorize(Policy = "Over21Only")]
     public class HomeController : Controller
     {
         public IActionResult Index()
