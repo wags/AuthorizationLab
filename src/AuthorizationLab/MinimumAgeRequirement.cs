bfc923ca5e517b7f07d34f360249a1c402f66148
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-04 09:45:27 -0400

    Add custom requirement and handler class
    For simplicity, the requirement and the handler are in the same class.
    Normally authorization handler is in a separate class and gets
    registered with the dependency injection system that is build into
    ASP.NET and inject extra information from other systems.

---
+++ src/AuthorizationLab/MinimumAgeRequirement.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthorizationLab
+{
+    public class MinimumAgeRequirement : AuthorizationHandler<MinimumAgeRequirement>, IAuthorizationRequirement
+    {
+        int _minimumAge;
+
+        public MinimumAgeRequirement(int minimumAge)
+        {
+            _minimumAge = minimumAge;
+        }
+
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            MinimumAgeRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.DateOfBirth))
+            {
+                return Task.FromResult(0);
+            }
+
+            var dateOfBirth = Convert.ToDateTime(
+                context.User.FindFirst(c => c.Type == ClaimTypes.DateOfBirth).Value);
+
+            int calculatedAge = DateTime.Today.Year - dateOfBirth.Year;
+            if (dateOfBirth > DateTime.Today.AddYears(-calculatedAge))
+            {
+                calculatedAge--;
+            }
+
+            if (calculatedAge >= _minimumAge)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+}
