d1ae1a0a91b662ace51b090ca13c97352899887b
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-04 10:41:52 -0400

    Add multiple handlers for a requirement
    Example of an office entry environment, and either a valid badge or a
    valid temporary pass is required.

---
+++ src/AuthorizationLab/HasTemporaryPassHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthorizationLab
+{
+    public class HasTemporaryPassHandler : AuthorizationHandler<OfficeEntryRequirement>
+    {
+        protected override Task HandleRequirementAsync(
+          AuthorizationHandlerContext context,
+          OfficeEntryRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "TemporaryBadgeExpiry" &&
+                                            c.Issuer == "https://contoso.com"))
+            {
+                return Task.FromResult(0);
+            }
+
+            var temporaryBadgeExpiry =
+                Convert.ToDateTime(context.User.FindFirst(
+                                       c => c.Type == "TemporaryBadgeExpiry" &&
+                                       c.Issuer == "https://contoso.com").Value);
+
+            if (temporaryBadgeExpiry > DateTime.Now)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+}
