8cda401aa6b730adca10dfe65bffc767d0e5ae04
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-04 10:47:20 -0400

    Add policy and register handlers
    Because the policy requirement is separate from the handlers, the
    handlers have to be registered in the DI container manually.

--- src/AuthorizationLab/Startup.cs
+++ src/AuthorizationLab/Startup.cs
@@ -22,6 +22,7 @@ namespace AuthorizationLab
                 options.AddPolicy("AdministratorOnly", policy => policy.RequireRole("Administrator"));
                 options.AddPolicy("EmployeeId", policy => policy.RequireClaim("EmployeeId", "123", "456"));
                 options.AddPolicy("Over21Only", policy => policy.Requirements.Add(new MinimumAgeRequirement(21)));
+                options.AddPolicy("BuildingEntry", policy => policy.Requirements.Add(new OfficeEntryRequirement()));
             });
 
             services.AddMvc(config =>
@@ -31,6 +32,9 @@ namespace AuthorizationLab
                                  .Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
+
+            services.AddSingleton<IAuthorizationHandler, HasBadgeHandler>();
+            services.AddSingleton<IAuthorizationHandler, HasTemporaryPassHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
